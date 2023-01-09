48c942cb54aae06128f678dbee40dc40ed7fbb31
Artem Drushchyts <artem.drushchyts@tangiblee.com>
2023-01-09 23:32:45 +0300

    feat: implement a simple server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,15 @@
-console.log([1,2,3] as number[])
+import * as dotenv from 'dotenv';
+import { createServer } from 'http';
+
+dotenv.config();
+
+const port = process.env.PORT;
+const server = createServer();
+
+const startServer = () => {
+    server.listen(port, () => {
+        console.log(`Server run on PORT: ${port}`)
+    });
+}
+
+startServer();
